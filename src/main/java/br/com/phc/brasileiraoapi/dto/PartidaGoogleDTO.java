371a36d36158bb21d667f6e960dfa37e40d982b0
Walison Martins <walisonmartins2009@hotmail.com>
2022-11-24 20:43:30 -0300

    Criacao do dto com dados de partida, criacao do enunm de status da partida, e ajustes.

---
+++ src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
@@ -0,0 +1,34 @@
+package br.com.phc.brasileiraoapi.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartidaGoogleDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String statuspartida;
+	private String tempoPartida;
+	// informações da equipe da casa
+	private String nomeEquipeCasa;
+	private String urlLogoEquipeCasa;
+	private Integer placarEquipeCasa;
+	private String golsEquipeCasa;
+	private String placarEstendidoEquipeCasa;
+	// informações da equipe da visitante
+	private String nomeEquipeVisistante;
+	private String urlLogoEquipeVisistante;
+	private Integer placarEquipeVisistante;
+	private String golsEquipeVisistante;
+	private String placarEstendidoEquipeVisistante;
+
+	
+}
